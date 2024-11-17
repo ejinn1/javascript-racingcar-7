66e6c327aab987dafee2e70b7800e78cda1f5ae6
ejinn1 <ejinn1@naver.com>
2024-11-17 22:38:59 +0900

    feat: prompt car names

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Input from './views/Input.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputView = new Input();
+
+    const carNames = await inputView.propmtCarName();
+  }
 }
 
 export default App;
